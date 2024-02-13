7510fb3d1a55925bb291e745efdaaa937840748d
Sergey Toporets <yateperna@yandex.ru>
2024-02-13 13:27:22 +0300

    Переписал класс RabbitMQ

--- web_api/adapters/broker/rabbit/rabbit_storage.py
+++ web_api/adapters/broker/rabbit/rabbit_storage.py
@@ -1,12 +1,11 @@
 "Модуль для создания подключения к RabbitMQ."
 
-from typing import Optional
-
 import aio_pika
 from fastapi import Depends
 from pydantic import BaseModel
 
 from adapters.broker.rabbit.rabbit_di import get_rabbit
+from core.config import settings
 
 
 class RabbitMQ:
@@ -15,35 +14,32 @@ class RabbitMQ:
     def __init__(self, connection: aio_pika.RobustConnection) -> None:
         """Инициализация объекта."""
         self.connection = connection
-        self.channel: Optional[aio_pika.RobustChannel] = None
-        self.exchange: Optional[aio_pika.Exchange] = None
+        self.channel: aio_pika.RobustChannel | None = None
+        self.exchange: aio_pika.Exchange | None = None
 
-    async def create_queue_and_bind(self):
+    async def create_queue_and_bind(self) -> None:
         """Асинхронная инициализация обменника в брокере."""
         self.channel = await self.connection.channel()
-        # self.exchange = await self.channel.get_exchange('topic_v1')
         self.exchange = await self.channel.declare_exchange(
             name='topic_v1',
             type='topic',
-            # auto_delete=True
         )
-        queue = await self.channel.declare_queue('queue_name', durable=True)
-        await queue.bind(self.exchange, 'routing_key')
+        queue = await self.channel.declare_queue(settings.broker.rabbit_queue, durable=True)
+        await queue.bind(self.exchange, 'events.files')
 
     async def publish(self, msg: BaseModel, routing_key: str, priority: int | None = None):
         """Публикация сообщения в брокере."""
         await self.exchange.publish(
-            aio_pika.Message(body=msg.json().encode(), priority=priority),
-            routing_key=routing_key
+            aio_pika.Message(
+                body=msg.json().encode(),
+                priority=priority,
+                delivery_mode=aio_pika.DeliveryMode.PERSISTENT),
+            routing_key=routing_key,
         )
         print(f'Message {msg} отправлено', f' в очередь {routing_key}')
 
 
-from loguru import logger
-# async def get_broker(connection: aio_pika.RobustConnection = Depends(get_rabbit)):
-async def get_broker():
-    connection = await get_rabbit()
-    logger.info(connection)
+async def get_broker(connection: aio_pika.RobustConnection = Depends(get_rabbit)):
     broker = RabbitMQ(connection)
     await broker.create_queue_and_bind()
     return broker
