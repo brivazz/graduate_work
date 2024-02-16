5ef396936139f3952df0edf9dc6bf9114fb052f5
Sergey Toporets <yateperna@yandex.ru>
2024-02-16 10:09:39 +0300

    Добавил docstring и аннотации типов в базовой модели

--- web_api/models/base.py
+++ web_api/models/base.py
@@ -1,12 +1,16 @@
+from typing import Any
+
 import orjson
 from pydantic import BaseModel
 
 
-def orjson_dumps(v, *, default):
+def orjson_dumps(v: Any, *, default: Any) -> str:
+    # orjson.dumps возвращает байты, соответствующие стандартному json.dumps, которые нам нужно декодировать
+    # берется из https://docs.pydantic.dev/usage/exporting_models/#custom-json-deserialisation
     return orjson.dumps(v, default=default).decode()
 
 
 class BaseOrjsonModel(BaseModel):
     class Config:
-        json_loads = orjson.loads
-        json_dumps = orjson_dumps
+        model_load_json = orjson.loads
+        model_dump_json = orjson_dumps
